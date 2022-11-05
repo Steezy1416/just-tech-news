fbbbda85cdd8fcea999d39d13c1d58091e1c0929
alexfortin <fortin.alex14@gmail.com>
2022-11-04 20:44:10 -0500

    passwords are now hashed

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const {Model, DataTypes} = require("sequelize")
 const sequelize = require("../config/connection")
+const bcrypt = require("bcrypt")
 
 //create a user model
 class User extends Model {}
@@ -34,11 +35,24 @@ User.init(
             validate: {
                 len: [4]
             }
-        }
+        },
 
     },
     //table configurations
     {
+        hooks: {
+            //set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData){
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData
+            },
+
+            //set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updateUserData) {
+                updateUserData.password = await bcrypt.hash(updateUserData.password, 10)
+                return updateUserData
+            }
+        },
         //pass in the connection to database
         sequelize,
         //dont create createdAt/updatedAt timestamp fields
